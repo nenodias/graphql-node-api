e1610b44cc90ed2cd07a2fd758b747f1728c2047
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-01-11 18:03:27 -0200

    Mostrando os resolvers triviais

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -23,11 +23,29 @@ const typeDefs = `
     type Query {
         allUsers: [User!]!
     }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User
+    }
+
 `;
 
 const resolvers = {
+    // Resolvers triviais
+    // User: {
+    //     id: (user) => user.id,
+    //     name: (user) => user.name,
+    //     email: (user) => user.email
+    // },
     Query: {
         allUsers: () => users
+    },
+    Mutation: {
+        createUser: (parent, args, context, info) => {
+            const newUser = Object.assign({ id: users.length + 1 }, args);
+            users.push(newUser);
+            return newUser;
+        }
     }
 };
 
