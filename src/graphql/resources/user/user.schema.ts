5185ea8499c703128b9546ad0eae6fdbd7b9b98e
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-02 18:04:08 -0200

    Finalizando os posts resolvers

--- src/graphql/resources/user/user.schema.ts
+++ src/graphql/resources/user/user.schema.ts
@@ -8,6 +8,7 @@ const userTypes = `
         photo: String
         createtAt: String!
         updatedAt: String!
+        posts(first: Int, offset: Int): [ Post! ]!
     }
 
     input UserCreateInput {
