b3e7f826674ae6f45d10509168a7cf9796e10553
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-04 22:36:27 -0200

    Adicionando o middlewaree para extrair jwt

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import * as graphqlHTTP from 'express-graphql';
 import db from './models';
 
 import schema from './graphql/schema';
+import { extractJwtMiddleware } from './middlewares/extract-jwt.middleware';
 
 class App {
 
@@ -17,8 +18,9 @@ class App {
     private middleware(): void {
         this.express.use('/graphql', 
 
+            extractJwtMiddleware(),
+
             (req, res, next) => {
-                req['context'] = {};
                 req['context'].db = db;
                 next();
             },
