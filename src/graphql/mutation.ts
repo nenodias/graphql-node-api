fea1a2e98b1d54034e1ea022567981061f649c00
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-04 22:22:57 -0200

    Adicionando o token ao graphql

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,13 +1,16 @@
-import { postMutations } from './resources/post/post.schema';
-import { userMutations } from './resources/user/user.schema';
 import { commentMutations } from './resources/comment/comment.schema';
+import { postMutations } from './resources/post/post.schema';
+import { tokenMutations } from './resources/token/token.schema';
+import { userMutations } from './resources/user/user.schema';
 
 const Mutation = `
     type Mutation {
         ${commentMutations}
         ${postMutations}
+        ${tokenMutations}
         ${userMutations}
     }
 `;
 
 export { Mutation };
+
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -6,12 +6,15 @@ import { commentResolvers } from './resources/comment/comment.resolvers';
 import { commentTypes } from './resources/comment/comment.schema';
 import { postResolvers } from './resources/post/post.resolvers';
 import { postTypes } from './resources/post/post.schema';
+import { tokenResolvers } from './resources/token/token.resolvers';
+import { tokenTypes } from './resources/token/token.schema';
 import { userResolvers } from './resources/user/user.resolvers';
 import { userTypes } from './resources/user/user.schema';
 
 const resolvers = merge(
     commentResolvers,
     postResolvers,
+    tokenResolvers,
     userResolvers
 );
 
@@ -29,6 +32,7 @@ export default makeExecutableSchema({
         Mutation,
         commentTypes,
         postTypes,
+        tokenTypes,
         userTypes,
     ],
     resolvers
