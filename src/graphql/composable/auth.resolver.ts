963a8297096bd0505b2c86de8fe894bb1107848e
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-04 23:09:08 -0200

    Adcionando auth resolver

---
+++ src/graphql/composable/auth.resolver.ts
@@ -0,0 +1,14 @@
+import { GraphQLFieldResolver } from "graphql";
+import { ResolverContext } from "../../interfaces/ResolverContextInterface";
+import { ComposableResolver } from "./composable.resolver";
+
+export const authResolver: ComposableResolver<any, ResolverContext> =
+    (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
+        return (parent, args, context, info) => {
+            if(context.user || context.authorization){
+                return resolver(parent, args, context, info);
+            }
+
+            throw new Error('Unauthorized token not provided!');
+        }
+    };
