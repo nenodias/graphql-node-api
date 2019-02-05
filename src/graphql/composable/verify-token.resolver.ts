151535593426060a7dda028af1345688af8a8d0f
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-04 23:17:14 -0200

    formatando o auth.resolve

---
+++ src/graphql/composable/verify-token.resolver.ts
@@ -0,0 +1,19 @@
+import { GraphQLFieldResolver } from "graphql";
+import * as jwt from "jsonwebtoken";
+import { ResolverContext } from "../../interfaces/ResolverContextInterface";
+import { JWT_SECRET } from "../../utils/utils";
+import { ComposableResolver } from "./composable.resolver";
+
+export const verifyTokenResolver: ComposableResolver<any, ResolverContext> =
+    (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
+        return (parent, args, context: ResolverContext, info) => {
+            const token: string = context.authorization ? context.authorization.split(' ')[1] : undefined;
+            jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
+                if (!err) {
+                    return resolver(parent, args, context, info);
+                }
+
+                throw new Error(`${err.name}:${err.message}`);
+            });
+        }
+    }
