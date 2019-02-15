c151e3e4ec292ebef82b4fbcba299444f40df041
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-15 21:49:33 -0200

    Finalizando testes mutations comments

--- test/resources/comment/comment.resolver.test.ts
+++ test/resources/comment/comment.resolver.test.ts
@@ -104,6 +104,110 @@ describe('Comment', () => {
 
     describe('Mutations', () => {
         describe('application/json', () => {
+            describe('createComment', () => {
+                it('should create a new Comment', () => {
+                    let body = {
+                        query: `
+                            mutation createNewComment($input: CommentInput!) {
+                                createComment(input: $input) {
+                                    comment
+                                    user {
+                                        id
+                                        name
+                                    }
+                                    post {
+                                        id
+                                        title
+                                    }
+                                }
+                            }
+                        `,
+                        variables: {
+                            input: {
+                                comment: 'Fourth comment',
+                                post: postId
+                            }
+                        }
+                    };
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .set('authorization', `Bearer ${token}`)
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const createdComment = res.body.data.createComment;
+                            expect(res.body.data).to.be.an('object');
+                            expect(res.body.data).to.have.key('createComment');
+                            expect(createdComment).to.be.an('object');
+                            expect(createdComment).to.have.keys(['comment', 'user', 'post']);
+                            expect(parseInt(createdComment.user.id)).to.equal(userId);
+                            expect(createdComment.user.name).to.equal('Peter Quill');
+                            expect(parseInt(createdComment.post.id)).to.equal(postId);
+                            expect(createdComment.post.title).to.equal('First Post');
+                        }).catch(handleError);
+                });
+            });
+
+            describe('updateComment', () => {
+                it('should update an existing Comment', () => {
+                    let body = {
+                        query: `
+                            mutation updateExistingComment($id: ID!, $input: CommentInput!) {
+                                updateComment(id: $id, input: $input) {
+                                    id
+                                    comment
+                                }
+                            }
+                        `,
+                        variables: {
+                            id: commentId,
+                            input: {
+                                comment: 'Comment changed',
+                                post: postId
+                            }
+                        }
+                    };
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .set('authorization', `Bearer ${token}`)
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const updatedComment = res.body.data.updateComment;
+                            expect(res.body.data).to.be.an('object');
+                            expect(res.body.data).to.have.key('updateComment');
+                            expect(updatedComment).to.be.an('object');
+                            expect(updatedComment).to.have.keys(['id','comment']);
+                            expect(updatedComment.comment).to.be.equal('Comment changed');
+                        }).catch(handleError);
+                });
+            });
+
+            describe('deleteComment', () => {
+                it('should delete an existing Comment', () => {
+                    let body = {
+                        query: `
+                            mutation deleteExistingComment($id: ID!) {
+                                deleteComment(id: $id)
+                            }
+                        `,
+                        variables: {
+                            id: commentId
+                        }
+                    };
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .set('authorization', `Bearer ${token}`)
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            const deletedComment = res.body.data.deleteComment;
+                            expect(res.body.data).to.be.an('object');
+                            expect(res.body.data).to.have.key('deleteComment');
+                            expect(deletedComment).to.be.true;
+                        }).catch(handleError);
+                });
+            });
         });
     });
 
