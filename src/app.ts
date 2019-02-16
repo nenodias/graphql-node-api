f7d0128d4b48561363eec2dbecce6a9759ba40c5
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-02-15 23:00:49 -0200

    Adicionando o helmet

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 import * as cors from 'cors';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 import db from './models';
 
@@ -37,6 +38,7 @@ class App {
         }));
 
         this.express.use(compression());
+        this.express.use(helmet());
 
         this.express.use('/graphql', 
 
